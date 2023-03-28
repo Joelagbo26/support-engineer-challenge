51ba7f353ce8b37381557e29d423317addd8a22d
Joel Agbo <joelagbo26@gmail.com>
2023-03-28 07:12:02 -0500

    Update application_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout "mailer"
+
+  class ExampleMailer < ApplicationMailer
+  def send_email(to, subject, body)
+    mail(to: to, subject: subject, body: body)
+  end
 end
